e50c913965540918608973cbd08eccde5e1ce91e
mohamedsobhi70 <sobhi7070@gmail.com>
2025-01-06 23:09:02 +0200

    add 404 Page

--- app/not-found.js
+++ app/not-found.js
@@ -1,11 +1,27 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NotFound() {
     return (
-        <div className='flex flex-col justify-center items-center gap-8'>
-            <h2 className='text-7xl font-bold uppercase text-center'>Not Found</h2>
-            <p className='text-2xl text-[#555] capitalize text-center'>Could not find requested resource</p>
-            <Link href="/" className='border border-[#ccc] rounded-2xl px-8 py-3.5 text-xl uppercase tracking-widest'>Return Home</Link>
-        </div>
+        <section className='py-16 lg:py-24'>
+            <div className='flex flex-col justify-center items-center gap-12'>
+                <Image src="/assets/images/404.png" width="592" height="395" alt="page-not-found" />
+                <div className="flex flex-col gap-3">
+                    <h2 className='section-title text-center font-black capitalize'>
+                        Uh-Oh! Looks like you are lost.
+                    </h2>
+                    <p className='section-exc text-center'>
+                        آسفين! الصفحة اللي تبحث عنها غير موجودة بالوقت الحالى
+                    </p>
+                </div>
+                <div className="flex flex-col md:flex-row gap-3 lg:gap-4">
+                    <Link href="/" className="btn-lg btn-primary">
+                        الصفحة الرئيسية
+                    </Link>
+                    <Link className="btn-lg btn-secondary" href="/support">الدعم الفنى</Link>
+                </div>
+            </div>
+
+        </section>
     )
 }
